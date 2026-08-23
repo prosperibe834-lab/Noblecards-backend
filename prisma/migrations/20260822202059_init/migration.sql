4926f63d92d5450b14bba68d0741321ef4d1c507
prosper ibe <USER PC@ghostgirl.ghostgirl>
2026-08-23 21:48:48 +0100

    Initial NobleCards NestJS backend

---
+++ prisma/migrations/20260822202059_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT,
+    "country" TEXT,
+    "countryCode" TEXT,
+    "gender" TEXT,
+    "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "transactionPinHash" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
